d78af7bf9242a8c381bcbacfe07fb23b91ff06d5
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2023-03-08 09:55:44 +0100

    Testing performance

--- examples/etest.jl
+++ examples/etest.jl
@@ -1,5 +1,7 @@
 import InteractiveIsing: connW, connIdx
 
+using LoopVectorization
+
 function getENorm(g, gstate, gadj, idx, htype)::Float32
     efac = Float32(0)
     @inbounds for conn_idx in eachindex(gadj[idx])
@@ -15,6 +17,17 @@ function getENormSimd(g, gstate, gadj, idx, htype)::Float32
     @inbounds @simd for conn_idx in eachindex(gadj[idx])
         conn = gadj[idx][conn_idx]
         efac += -connW(conn) * gstate[connIdx(conn)]
+    end
+
+    return efac
+end
+
+function getENormTurbo(g, gstate, gadj, idx, htype)::Float32
+    efac::Float32 = Float32(0)
+    idxs = gadj[idx][1]
+    weights = gadj[idx][2]
+    @inbounds @turbo for idx in eachindex(idxs)
+        efac += -gstate[idxs[idx]]*weights[idx]
     end
 
     return efac
@@ -40,3 +53,36 @@ function getENormMag(g, gstate, gadj, idx, htype)::Float32
     end
     return efac - mlist(g)[idx]
 end
+
+getENormSum(g, gstate, gadj, idx, htype) = sum(i -> -connW(gadj[idx][i]) * gstate[connIdx(gadj[idx][i])], 1:length(gadj[idx]))
+
+function convertAdj(adj)
+newadj::Vector{Tuple{Vector{Int32},Vector{Float32}}} = []
+
+    for idx in eachindex(adj)
+        idxentry::Vector{Int32} = []
+        connentry::Vector{Float32} = []
+        for entryidx in eachindex(adj[idx])
+            push!(idxentry, adj[idx][entryidx][1])
+            push!(connentry, adj[idx][entryidx][2])
+        end
+      
+        push!(newadj, tuple(idxentry,connentry))
+    end
+    return newadj
+end
+function convertAdj64(adj)
+    newadj::Vector{Tuple{Vector{Int64},Vector{Float64}}} = []
+    
+        for idx in eachindex(adj)
+            idxentry::Vector{Int64} = []
+            connentry::Vector{Float64} = []
+            for entryidx in eachindex(adj[idx])
+                push!(idxentry, adj[idx][entryidx][1])
+                push!(connentry, adj[idx][entryidx][2])
+            end
+          
+            push!(newadj, tuple(idxentry,connentry))
+        end
+        return newadj
+    end
