86dd8f7dde7817d83fa1739b2bdd60c070e5e63d
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2024-05-21 16:01:13 +0200

    Ising def

--- src/MCAlgorithms/Hamiltonians/Ising.jl
+++ src/MCAlgorithms/Hamiltonians/Ising.jl
@@ -6,7 +6,7 @@ params(::Type{Ising}, GraphType::Type)  = GatherHamiltonianParams((:b, Vector{Gr
 
 function Δi_H(::Type{Ising})
     collect_expr = :(w_ij*s_j)
-    return_expr = :((s_i^2-sn_i^2)*self_i+(s_i-sn_i)*(b+collect_expr))
+    return_expr = :((s_i^2-sn_i^2)*self_i+(s_i-sn_i)*(b_i+collect_expr))
     return HExpression(collect_expr, return_expr)
 end
 
