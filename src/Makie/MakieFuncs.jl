5c2e5f9bf3b9a68a58e2b146e8eb5d1b3180a83b
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2025-05-22 15:41:56 +0200

    markersize hack

--- src/Makie/MakieFuncs.jl
+++ src/Makie/MakieFuncs.jl
@@ -124,6 +124,7 @@ function create_layer_axis!(layer, panel_or_window; color = nothing, pos = (1,1)
     new_img!(g, layer, panel_or_window; color, colormap)
 end
 
+const makie_markersize = Ref(0.01)
 # Get image either for 2d or 3d
 function new_img!(g, layer, mp; color = nothing, colormap = :thermal)
     dims = length(size(layer))
@@ -150,7 +151,7 @@ function new_img!(g, layer, mp; color = nothing, colormap = :thermal)
         ys = idx2ycoord.(Ref(sz), allidxs)
         zs = idx2zcoord.(Ref(sz), allidxs)
         
-        mp["image"] = meshscatter!(ax, xs, ys, zs, markersize = 0.01, color = mp["obs"], colormap = colormap)
+        mp["image"] = meshscatter!(ax, xs, ys, zs, markersize = makie_markersize[], color = mp["obs"], colormap = colormap)
 
     end
         mp["image"].colorrange[] = (first(stateset(layer)), last(stateset(layer)))
