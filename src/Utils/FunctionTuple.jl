428e6c0372cad00c153c610f521616c3aa56a513
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2025-08-17 16:39:24 +0200

    Stuff

---
+++ src/Utils/FunctionTuple.jl
@@ -0,0 +1,20 @@
+@inline function func_tuple_unroll(fs, args::Union{Nothing, Tuple}) where N
+    tuple(_func_tuple_unroll(gethead(fs), gettail(fs), args)...)
+end
+
+@inline function _func_tuple_unroll(fhead, ftail, args)
+    if isnothing(args)
+        if isempty(ftail)
+           return (fhead(),)
+        else
+           return (fhead(), _func_tuple_unroll(gethead(ftail), gettail(ftail), args)...)
+        end
+    else
+        if isempty(ftail)
+            return (fhead(args...),)
+        else
+            return (fhead(args...), _func_tuple_unroll(gethead(ftail), gettail(ftail), args)...)
+        end
+    end
+end
+
