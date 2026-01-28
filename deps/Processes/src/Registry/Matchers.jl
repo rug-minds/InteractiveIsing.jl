0bf96ed5ca00bb4dcbd572ef2d0b041810175e11
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2026-01-28 15:41:39 +0100

    comment out matchers

--- deps/Processes/src/Registry/Matchers.jl
+++ deps/Processes/src/Registry/Matchers.jl
@@ -1,9 +1,9 @@
-export All
-struct All{T} end
-All(::Type{T}) where {T} = All{T}()
-gettype(::All{T}) where {T} = T
+# export All
+# struct All{T} end
+# All(::Type{T}) where {T} = All{T}()
+# gettype(::All{T}) where {T} = T
 
-function static_get(reg:::AbstractRegistry, obj::All{T}) where {T}
-    type_entries = get_type_entries(reg, T)
-    return tuple(type_entries...)
-end
+# function static_get(reg:::AbstractRegistry, obj::All{T}) where {T}
+#     type_entries = get_type_entries(reg, T)
+#     return tuple(type_entries...)
+# end
