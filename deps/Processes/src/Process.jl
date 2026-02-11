add6246f4609f24adcdbe80eb629764aa7b6234b
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2026-02-11 17:28:50 +0100

    TimedProcessinit

--- deps/Processes/src/Process.jl
+++ deps/Processes/src/Process.jl
@@ -69,7 +69,9 @@ function Process(func::Union{ProcessAlgorithm, LoopAlgorithm}, inputs_overrides.
 
     # context = init_context(td)
 
-    context = prepare_context(td)
+    if isnothing(context)
+         context = prepare_context(td)
+    end
     # p = Process(uuid1(), context, td, timeout, nothing, UInt(1), UInt(1), Threads.ReentrantLock(), false, true, nothing, nothing, Arena(), RuntimeListeners(), 0)
     p = Process(uuid1(), context, td, timeout, nothing, UInt(1), UInt(1), Threads.ReentrantLock(), false, true, nothing, nothing, RuntimeListeners(), 0)
 
--- deps/Processes/src/TimedProcess.jl
+++ deps/Processes/src/TimedProcess.jl
@@ -2,4 +2,6 @@ struct TimedProcess{TD, C} <: AbstractProcess
     timer::Base.RefValue{Timer, Nothing}
     taskdata::TD
     context::C
+    paused::Base.RefValue{Bool}
 end
+
