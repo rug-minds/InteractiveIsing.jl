eb22ee0b613a483a6cea8ff237715d5b151e70bd
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2026-02-27 15:16:50 +0100

    Correctly set self

--- src/MCAlgorithms/Hamiltonians/Terms/Quartic.jl
+++ src/MCAlgorithms/Hamiltonians/Terms/Quartic.jl
@@ -1,10 +1,11 @@
 export Quartic
-struct Quartic{PV <: ParamTensor} <: HamiltonianTerm 
+struct Quartic{S, PV <: ParamTensor} <: HamiltonianTerm
+    self::S
     qc::PV
 end
 
 # Quartic holds a 0-dimensional (e.g. scalar) ParamTensor
-Quartic(g::AbstractIsingGraph, val = 1) = Quartic(ScalarParam(eltype(g), val; description = "Quartic Coefficient"))
+Quartic(g::AbstractIsingGraph, val = 1) = Quartic(g.self, ScalarParam(eltype(g), val; description = "Quartic Coefficient"))
 
 # @inline @Auto_ΔH function ΔH(::Quartic, hargs, proposal)
 #     return :(qc[]*self[j]*s[j]^4)
--- src/MCAlgorithms/Hamiltonians/Terms/Sextic.jl
+++ src/MCAlgorithms/Hamiltonians/Terms/Sextic.jl
@@ -1,10 +1,11 @@
 export Sextic
-struct Sextic{PV <: ParamTensor} <: HamiltonianTerm 
+struct Sextic{S, PV <: ParamTensor} <: HamiltonianTerm 
+    self::S
     sc::PV
 end
 
 # Sextic holds a 0-dimensional (e.g. scalar) ParamTensor
-Sextic(g::AbstractIsingGraph, val = 1) = Sextic(ScalarParam(eltype(g), val; description = "Sextic Coefficient"))
+Sextic(g::AbstractIsingGraph, val = 1) = Sextic(g.self, ScalarParam(eltype(g), val; description = "Sextic Coefficient"))
 
 # ΔH_expr[Sextic] = :(sc[]*self[j]*s[j]^6)
 # @inline @Auto_ΔH function ΔH(::Sextic, hargs, proposal)
