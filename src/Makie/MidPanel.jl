2de36d460f308355ec850f44b4db68ca810bbecc
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2026-03-20 14:20:16 +0100

    Removed left panel

--- src/Makie/MidPanel.jl
+++ src/Makie/MidPanel.jl
@@ -13,82 +13,82 @@ function midPanel(window)
     midpanel(ml, LayoutPanel(midgrid))
     mp = midpanel(ml)
 
-    # LEFT Panel
-    mp[:leftpanel] = leftpanel = GridLayout(midgrid[1,1], tellheight = false)
+    # # LEFT Panel
+    # mp[:leftpanel] = leftpanel = GridLayout(midgrid[1,1], tellheight = false)
 
-            # LEFT PANEL 1,1
-            mp[:buttons] = buttons = GridLayout(leftpanel[1,1])
+    #         # LEFT PANEL 1,1
+    #         mp[:buttons] = buttons = GridLayout(leftpanel[1,1])
 
-            # Brush buttons
-                mp[:bs] = bs = [Button(buttons[-i+2,1], padding = (0,0,0,0), fontsize = 24, width = 40, height = 40, label = "$i") for i in 1:-1:-1]
+    #         # Brush buttons
+    #             mp[:bs] = bs = [Button(buttons[-i+2,1], padding = (0,0,0,0), fontsize = 24, width = 40, height = 40, label = "$i") for i in 1:-1:-1]
 
-                for (idx,val) in enumerate(1:-1:-1)
-                    push!(obs_funcs, on(bs[idx].clicks) do _
-                        # println("clicked $val")
-                        brush(simulation)[] = Float32(val)
-                    end)
-                end
+    #             for (idx,val) in enumerate(1:-1:-1)
+    #                 push!(obs_funcs, on(bs[idx].clicks) do _
+    #                     # println("clicked $val")
+    #                     brush(simulation)[] = Float32(val)
+    #                 end)
+    #             end
 
-            # Clamp toggle
-                mp[:clamplabel] = Label(buttons[4,1], "Clamping", fontsize = 18)
+    #         # Clamp toggle
+    #             mp[:clamplabel] = Label(buttons[4,1], "Clamping", fontsize = 18)
 
-                mp[:clamptoggle] = clamptoggle = Toggle(buttons[5,1], active = false)
+    #             mp[:clamptoggle] = clamptoggle = Toggle(buttons[5,1], active = false)
 
-            # LEFT PANEL 2,2
-            # SIZE TEXTBOX 
-                mp[:sizetextbox] = size_grid = GridLayout(leftpanel[2,1])
+    #         # LEFT PANEL 2,2
+    #         # SIZE TEXTBOX 
+    #             mp[:sizetextbox] = size_grid = GridLayout(leftpanel[2,1])
 
-                size_validator(r_string) = try 0 < parse(UInt, r_string) < sim_max_r(window); catch; false; end
-                mp[:size_label_text] = sl_text = lift(x -> "Radius < $(sim_max_r(window))", window[:layer_idx])
-                mp[:sizelabel] = Label(size_grid[1,1], sl_text)
-                mp[:sizefield] = sizefield = UIntTextbox(size_grid[2,1], 
-                    onfunc = (num) -> window[:brush_r][] = num,
-                    placeholder = "$(window[:brush_r][])",
-                    upper = () -> sim_max_r(window), 
-                    width = 40, 
-                    defocus_on_submit = true, 
-                    reset_on_defocus = true)
+    #             size_validator(r_string) = try 0 < parse(UInt, r_string) < sim_max_r(window); catch; false; end
+    #             mp[:size_label_text] = sl_text = lift(x -> "Radius < $(sim_max_r(window))", window[:layer_idx])
+    #             mp[:sizelabel] = Label(size_grid[1,1], sl_text)
+    #             mp[:sizefield] = sizefield = UIntTextbox(size_grid[2,1], 
+    #                 onfunc = (num) -> window[:brush_r][] = num,
+    #                 placeholder = "$(window[:brush_r][])",
+    #                 upper = () -> sim_max_r(window), 
+    #                 width = 40, 
+    #                 defocus_on_submit = true, 
+    #                 reset_on_defocus = true)
 
-                # TODO: Restore
-                # push!(obs_funcs, on(window[:brush_r]) do x
-                #     sizefield.placeholder[] = string(x)
-                #     #Set circle
-                #     circ(simulation, getOrdCirc(window[:brush_r][]))
-                # end)
+    #             # TODO: Restore
+    #             # push!(obs_funcs, on(window[:brush_r]) do x
+    #             #     sizefield.placeholder[] = string(x)
+    #             #     #Set circle
+    #             #     circ(simulation, getOrdCirc(window[:brush_r][]))
+    #             # end)
 
-            # SHOW BFIELD
-            mp[:showbfield] = showbfield = Toggle(leftpanel[4,1], active = false)
-            mp[:showbfieldlabel] = Label(leftpanel[3,1], "Show BField", fontsize = 18)
+    #         # SHOW BFIELD
+    #         mp[:showbfield] = showbfield = Toggle(leftpanel[4,1], active = false)
+    #         mp[:showbfieldlabel] = Label(leftpanel[3,1], "Show BField", fontsize = 18)
 
-            push!(obs_funcs, on(showbfield.active) do x
-                mp[:img_obs][] = getSingleViewImg(window)
-            end)
+    #         push!(obs_funcs, on(showbfield.active) do x
+    #             mp[:img_obs][] = getSingleViewImg(window)
+    #         end)
         
 
-            # SIZE SLIDER
-                # leftpanel[1,2] = slidergrid = GridLayout(tellheight = false)
+    #         # SIZE SLIDER
+    #             # leftpanel[1,2] = slidergrid = GridLayout(tellheight = false)
 
-                # mp["rslider_text"] = lift(x -> "r: $x", brushR(simulation))
-                # # Pop this listener when the label is removed
-                # push!(poplist, brushR(simulation))
+    #             # mp["rslider_text"] = lift(x -> "r: $x", brushR(simulation))
+    #             # # Pop this listener when the label is removed
+    #             # push!(poplist, brushR(simulation))
 
-                # Box(slidergrid[1,1], width = 100, height = 50, visible = false)
+    #             # Box(slidergrid[1,1], width = 100, height = 50, visible = false)
 
-                # mp["rsliderlabel"] = slidergrid[1,1] = Label(f, mp["rslider_text"] , fontsize = 18, width = 32, tellwidth = false)
+    #             # mp["rsliderlabel"] = slidergrid[1,1] = Label(f, mp["rslider_text"] , fontsize = 18, width = 32, tellwidth = false)
 
-                # mp["rslider"] = rslider = Slider(slidergrid[2,1], range = 1:100, value = 20, horizontal = false)
+    #             # mp["rslider"] = rslider = Slider(slidergrid[2,1], range = 1:100, value = 20, horizontal = false)
 
 
-                # on(rslider.value) do x
-                #     brushR(simulation)[] = Int32(rslider.value[])
-                # end
+    #             # on(rslider.value) do x
+    #             #     brushR(simulation)[] = Int32(rslider.value[])
+    #             # end
 
-                # # on(value_dragstop(rslider)) do _
-                # #     circ(simulation, getOrdCirc(brushR(simulation)[]))
-                # # end
+    #             # # on(value_dragstop(rslider)) do _
+    #             # #     circ(simulation, getOrdCirc(brushR(simulation)[]))
+    #             # # end
 
-                # # Initialize r and circle
-                # value_dragstop(rslider)[] = rslider.value[] = floor(Int64, min(size(currentLayer(simulation))...)/8)
+    #             # # Initialize r and circle
+    #             # value_dragstop(rslider)[] = rslider.value[] = floor(Int64, min(size(currentLayer(simulation))...)/8)
 
 
     # RIGHT PANEL
