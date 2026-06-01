0f982911e185e6f50c0f5eec46dfa3ee1d1e202c
Fabian IJpelaar <87425229+f-ij@users.noreply.github.com>
2026-06-01 15:45:10 +0200

    Back to main

--- deps/Processes/src/ProcessEntities/ProcessEntities.jl
+++ deps/Processes/src/ProcessEntities/ProcessEntities.jl
@@ -3,7 +3,6 @@ const ProcessEntity = Union{ProcessState, ProcessAlgorithm}
 init(::ProcessEntity, context) = (;)
 step!(pe::ProcessEntity, context) = error("step! not implemented for $(typeof(pe))")
 cleanup(::ProcessEntity, context) = (;)
-_step!(args...) = error("_step! not implemented for $(typeof(first(args)))")
 
 include("Matching.jl")
 include("Utils.jl")
--- deps/Processes/test/runtests.jl
+++ deps/Processes/test/runtests.jl
@@ -3,16 +3,14 @@ using Processes
 
 @testset "Processes" begin
     include("CompositeCompositionTest.jl")
-    # Out of scope for immutable_fix_manual while the old generated path is removed.
-    # include("InlineBenchmarkTest.jl")
+    include("InlineBenchmarkTest.jl")
     include("InlineProcessConstructorTest.jl")
     include("LifetimeTest.jl")
     include("FibLucProcessTest.jl")
     include("CopyManagerTest.jl")
     include("ProcessManagerTest.jl")
     include("RuntimeInputsLifecycleTest.jl")
-    # Package/SubPackage execution is out of scope for this branch.
-    # include("PackageTest.jl")
+    include("PackageTest.jl")
     include("RouteWalkerTest.jl")
     include("ShareContextTest.jl")
     include("RoutingWiringTest.jl")
@@ -21,8 +19,7 @@ using Processes
     include("SymbolIndexingTest.jl")
     include("InnerTypeFilterTest.jl")
     include("ContextShowingTest.jl")
-    # ContextInjector and interactive widget tooling are out of scope for this branch.
-    # include("ContextInjectorTest.jl")
+    include("ContextInjectorTest.jl")
     include("CompositeDSLTest.jl")
     include("ProcessAlgorithmMacroTest.jl")
     include("ContextAnalyzerTest.jl")
